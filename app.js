2d18dc62a96a3e6264324f8c06261d5855bd3f11
sochacki111 <michalsochacki111@gmail.com>
2020-06-17 20:31:18 +0200

    fixed indents

--- app.js
+++ app.js
@@ -10,16 +10,16 @@ const harAnalyzerRoutes = require('./routes/harAnalyzer');
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.json({
-  extended: true
+    extended: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
-  res.locals.cookies = req.cookies;
-  res.locals.testObject = req.testObject;
-  next();
+    res.locals.cookies = req.cookies;
+    res.locals.testObject = req.testObject;
+    next();
 });
 app.use('/', harAnalyzerRoutes);
 
 app.listen(PORT, () => {
-  console.log(`HTTP Archive Analyzer Server has started on port ${PORT}!`);
+    console.log(`HTTP Archive Analyzer Server has started on port ${PORT}!`);
 });
