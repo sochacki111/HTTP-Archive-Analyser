8f852e576463fbef53bbffb2a1912d07cbff0448
sochacki111 <michal.sochacki@billennium.com>
2020-06-17 20:06:18 +0200

    Updated listening port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const harAnalyzerRoutes = require('./routes/harAnalyzer');
 
 app.set('view engine', 'ejs');
